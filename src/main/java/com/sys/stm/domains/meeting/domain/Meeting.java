2acddd495ea31c6c3916b237f261eabe3b1620ba
baejiwon <qowhxk@naver.com>
2025-09-05 17:45:34 +0900

    feat : SYSONE-23 회의록 Entity 추가

--- src/main/java/com/sys/stm/domains/meeting/domain/Meeting.java
+++ src/main/java/com/sys/stm/domains/meeting/domain/Meeting.java
@@ -1,9 +1,39 @@
 package com.sys.stm.domains.meeting.domain;
 
-import lombok.Builder;
-import lombok.Data;
+import com.sys.stm.domains.meeting.dto.request.MeetingCreateRequest;
+import com.sys.stm.global.common.entity.BaseEntity;
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @Builder
 @Data
-public class Board {
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Meeting extends BaseEntity {
+
+    private Long id;                    // 회의록 ID (PK)
+    private Long projectId;             // 프로젝트 ID (FK)
+    private String title;               // 회의 제목
+    private String content;             // 회의 내용
+    private Long memberId;              // 회의록 작성자 (FK)
+    private String type;                // 회의 종류
+    private Timestamp progressDate;     // 회의 진행시간
+    private String place;               // 회의 장소
+
+    // Participant와의 관계 (1:N)
+    private List<MeetingParticipant> participants;
+
+    // 생성자에서 BaseEntity 초기화
+    public Meeting(MeetingCreateRequest request, Long projectId, Long memberId) {
+        this.projectId = projectId;
+        this.title = request.getTitle();
+        this.content = request.getContent();
+        this.memberId = memberId;
+        this.type = request.getType().toString();
+        this.progressDate = request.getProgressDate();
+        this.place = request.getPlace();
+    }
 }
