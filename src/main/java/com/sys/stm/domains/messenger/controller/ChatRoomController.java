04e5af0559c902fc214e308f52d0eebede0ef876
YoonYn9915 <jsy440720@gmail.com>
2025-09-08 06:14:41 +0900

    feat: SYSONE-17 채팅방 생성 관련 컨트롤러 클래스 구현

--- src/main/java/com/sys/stm/domains/messenger/controller/ChatRoomController.java
+++ src/main/java/com/sys/stm/domains/messenger/controller/ChatRoomController.java
@@ -1,6 +1,9 @@
 package com.sys.stm.domains.messenger.controller;
 
 
+import com.sys.stm.domains.messenger.domain.ChatRoom;
+import com.sys.stm.domains.messenger.domain.ChatRoomParticipant;
+import com.sys.stm.domains.messenger.dto.request.ChatRoomCreateRequestDto;
 import com.sys.stm.domains.messenger.dto.response.ChatRoomDataResponseDto;
 import com.sys.stm.domains.messenger.service.ChatRoomParticipantService;
 import com.sys.stm.domains.messenger.service.ChatRoomService;
@@ -25,6 +28,25 @@ public class ChatRoomController {
         //TODO SecurityContextHolder에 있는 Member 객체 가져오기, 일단 지금은 member id 하드코딩
         long id = 1;
         return ApiResponse.ok(chatRoomService.findAllChatRoomsDataById(id));
+    }
+
+    // 채팅방 생성
+    @PostMapping("/create-chat-room")
+    public ApiResponse<String> createChatRoom(@RequestBody ChatRoomCreateRequestDto chatRoomCreateRequestDto) {
+
+        // 채팅방 검증(1. 1:1 메시지인데 채팅방 참여 인원이 여러 명인지, 2. 채팅방 참여 인원 id 리스트가 실제로 존재하는 사람인지)
+        chatRoomService.validateChatRoomData(chatRoomCreateRequestDto);
+
+        // 채팅방 생성
+        ChatRoom chatRoom = chatRoomCreateRequestDto.toChatRoom();
+        chatRoomService.createChatRoom(chatRoom);
+
+        // 채팅방에 초대된 사원 리스트 저장
+        List<ChatRoomParticipant> chatRoomParticipants = chatRoomCreateRequestDto.toChatRoomParticipants(chatRoom.getId());
+        chatRoomParticipantService.createChatRoomParticipants(chatRoomParticipants);
+
+        return ApiResponse.created();
+
     }
 
 }
