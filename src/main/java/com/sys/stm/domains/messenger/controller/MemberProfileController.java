5f3dd01b6a79ffbc7be7a449353907639e801caa
YoonYn9915 <jsy440720@gmail.com>
2025-09-08 06:04:11 +0900

    feat: SYSONE-17 전체 사원, 특정 사원 프로필 조회 컨트롤러 클래스 구현

--- src/main/java/com/sys/stm/domains/messenger/controller/MemberProfileController.java
+++ src/main/java/com/sys/stm/domains/messenger/controller/MemberProfileController.java
@@ -27,4 +27,18 @@ public class MemberProfileController {
         return ApiResponse.ok(200, memberProfileResponseDto, "사원 프로필 조회 요청이 성공적으로 처리되었습니다.");
     }
 
+    // 모든 사원 프로필 정보 조회
+    @GetMapping("/all-profiles")
+    public ApiResponse<List<MemberProfileResponseDto>> findAllMemberProfiles() {
+        List<MemberProfileResponseDto> memberProfileResponseDtoList = memberProfileService.findAllMemberProfiles();
+        return ApiResponse.ok(200, memberProfileResponseDtoList, "전체 사원 프로필 조회 요청이 성공적으로 처리되었습니다.");
+    }
+
+    // 이메일, 이름으로 특정 사원 프로필 정보 조회
+    @GetMapping("/profiles")
+    public ApiResponse<List<MemberProfileResponseDto>> findMemberProfilesByEmailOrName(@RequestParam(name = "keyword") String keyword) {
+        List<MemberProfileResponseDto> memberProfileResponseDtoList = memberProfileService.findMemberProfilesByEmailOrName(keyword);
+        return ApiResponse.ok(200, memberProfileResponseDtoList, "전체 사원 프로필 조회 요청이 성공적으로 처리되었습니다.");
+    }
+
 }
