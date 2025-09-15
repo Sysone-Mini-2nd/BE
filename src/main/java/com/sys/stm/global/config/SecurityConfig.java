1426a0625a4f944c33121b06e88e0eb0f5ce1dee
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2025-09-15 11:20:05 +0900

    fix: 권한 ADMIN -> MASTER

--- src/main/java/com/sys/stm/global/config/SecurityConfig.java
+++ src/main/java/com/sys/stm/global/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                 .requestMatchers("/favicon.ico").permitAll()
 
                 // ADMIN 권한이 필요한 경로
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/admin/**").hasRole("MASTER")
 
                 // 나머지는 인증 필요
                 .anyRequest().authenticated()
